e771fcc05878566bbccbac6df276d64877a8e579
Sahith Nallapareddy <sahith.reddy@gmail.com>
2024-08-04 06:30:00 -0400

    finish configuration setup

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import Koa, { ParameterizedContext } from "koa"
 import Router from "@koa/router"
 import bodyParser from "@koa/bodyparser"
 import { randomBytes, createHmac, timingSafeEqual } from "crypto"
+import { initializeApp, cert } from "firebase-admin/app"
+import { getFirestore, Firestore, FieldValue } from "firebase-admin/firestore"
+import { subscribe } from "diagnostics_channel"
+import { createTwitchClient, getSecret } from "./twitch_client"
 
 const app = new Koa()
 const router = new Router()
@@ -19,12 +23,26 @@ const MESSAGE_TYPE_REVOCATION = 'revocation';
 
 const HMAC_PREFIX = 'sha256=';
 
-function getSecret() {
-    if (!process.env.SECRET) {
-        throw new Error("no secret defined!")
+function setupFirebase() {
+    // production, use firebase with SA credentials passed from environment
+    if (process.env.SERVICE_ACCOUNT) {
+        const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT)
+        initializeApp({
+            credential: cert(serviceAccount)
+        })
+
     }
-    return process.env.SECRET;
+    // dev, use firebase emulator
+    else {
+        if (!process.env.FIRESTORE_EMULATOR_HOST) {
+            throw new Error("Firestore emulator is not running!")
+        }
+        initializeApp({ projectId: "dev" })
+    }
+    return getFirestore()
 }
+
+const db: Firestore = setupFirebase()
 
 function getHmacMessage(ctx: ParameterizedContext) {
     return (ctx.get(TWITCH_MESSAGE_ID) +
@@ -43,38 +61,10 @@ function verifyMessage(hmac: string, verifySignature: string) {
     return timingSafeEqual(Buffer.from(hmac), Buffer.from(verifySignature));
 }
 
-const TwitchRequester = () => {
-    let token = ""
-    const refreshToken = async () => {
-        const res = await fetch("https://id.twitch.tv/oauth2/token", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            body: `client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=client_credentials`
-        })
-        if (!res.ok) {
-            const text = await res.text()
-            throw new Error("could not refresh token: " + text)
-        }
-        const tokenResponse = await res.json()
-        token = tokenResponse.access_token
-    }
-    return {
-        requestTwitchApi: async function(fetcher: (token: string) => Promise<Response>): Promise<Response> {
-            if (!token) {
-                await refreshToken()
-            }
-            const res = await fetcher(token)
-            if (res.status === 401) {
-                await refreshToken()
-            }
-            return await fetcher(token)
-        }
-    }
-}
+const twitchClient = createTwitchClient()
 
-const twitchCall = TwitchRequester()
+type BroadcastConfigurationEvent = { channel_id: string, role?: string, id: string, timestamp: string, title_keyword: string }
+type BroadcastConfigurationResponse = { "BROADCAST_CONFIGURATION": Array<BroadcastConfigurationEvent> }
 
 type Subscription = { id: string, status: string, type: string, version: "1", condition: { broadcaster_user_id: string }, transport: { method: string, callback: string }, created_at: string, cost: number }
 type Event = { id: string, broadcaster_user_id: string, broadcaster_user_login: string, broadcaster_user_name: string, type: string, started_at: string }
@@ -89,112 +79,15 @@ type TwitchUserInformation = { data: Array<UserInfo> }
 type SubscriptionInfo = { id: string, status: string, type: string, version: string, cost: number, condition: { broadcaster_user_id: string }, transpoter: { method: "webhook", callback: string }, created_at: string }
 type TwitchSubscription = { data: Array<SubscriptionInfo>, total: number, total_cost: number, max_total_cost: number }
 
-type ConfigureRequest = { discord_server: string, twitch_url: string, event_type: string }
+type AddTwitchChannelRequest = { discord_server: string, twitch_url: string }
+type RemoveTwitchChannelRequest = { discord_server: string, twitch_url: string }
 
-router.post("/events", async (ctx, next) => {
-    const twitchEvent = ctx.request.body as StreamUpEvent
-    ctx.status = 200
-    await next()
-    const twitchUser = twitchEvent.event.broadcaster_user_id
-    const res = await twitchCall.requestTwitchApi(async (token) =>
-        await fetch(`https://api.twitch.tv/helix/channels?broadcaster_id=${twitchUser}`, {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Client-Id": `${process.env.CLIENT_ID}`
-            }
-        })
-    )
-    if (!res.ok) {
-        const t = await res.text()
-        throw new Error(`twitch call for ${twitchUser} failed ${t}`)
-    }
-    const twitchResponse = await res.json() as TwitchChannelInformation
-    if (twitchResponse.data.length === 0) {
-        throw new Error("no twitch channel information found")
-    }
-    const broadcaster = twitchResponse.data[0]
-    const broadcastTitle = broadcaster.broadcaster_name
-}).post("/configure", async (ctx, next) => {
-    const request = ctx.request.body as ConfigureRequest
-    const login = request.twitch_url.substring(request.twitch_url.lastIndexOf('/') + 1)
-    const res = await twitchCall.requestTwitchApi(async (token) =>
-        await fetch(`https://api.twitch.tv/helix/users?login=${login}}`, {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Client-Id": `${process.env.CLIENT_ID}`
-            }
-        })
-    )
-    if (!res.ok) {
-        const t = await res.text()
-        throw new Error(`Could not find ${login} on Twitch! ` + t)
-    }
-    const twitchResponse = await res.json() as TwitchUserInformation
-    if (twitchResponse.data.length === 0) {
-        throw new Error(`Could not find information on ${login} on Twitch!`)
-    }
-    const user = twitchResponse.data[0]
-    const broadcaster_id = user.id
-    if (request.event_type === "ADD_CHANNEL") {
-        // create subscription
-        const res = await twitchCall.requestTwitchApi(async (token) =>
-            await fetch("https://api.twitch.tv/helix/eventsub/subscriptions", {
-                method: "POST",
-                headers: {
-                    "Authorization": `Bearer ${token}`,
-                    "Client-Id": `${process.env.CLIENT_ID}`,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    type: "stream.online",
-                    version: "1",
-                    condition: {
-                        "broadcaster_user_id": broadcaster_id
-                    },
-                    transport: {
-                        method: "webhook",
-                        callback: "URL HERE",
-                        secret: getSecret()
-                    }
-                })
-            })
-        )
-        if (!res.ok) {
-            const t = await res.text()
-            throw new Error(`could not create twitch subscription for ${broadcaster_id}: ${t}`)
-        }
-        const subscription = await res.json() as TwitchSubscription
-        await fetch("https://snallabot-event-sender-b869b2ccfed0.herokuapp.com/post", {
-            method: "POST",
-            body: JSON.stringify({ key: "twitch_channels", event_type: request.event_type, delivery: "EVENT_SOURCE", channel_id: broadcaster_id, discord_server: request.discord_server }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        await fetch("https://snallabot-event-sender-b869b2ccfed0.herokuapp.com/post", {
-            method: "POST",
-            body: JSON.stringify({ key: broadcaster_id, event_type: "ADDED_TWITCH_CHANNEL", delivery: "EVENT_SOURCE", channel_id: broadcaster_id, discord_server: request.discord_server }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-    } else if (request.event_type === "REMOVE_CHANNEL") {
-        // potentially delete subscription
-        await fetch("https://snallabot-event-sender-b869b2ccfed0.herokuapp.com/post", {
-            method: "POST",
-            body: JSON.stringify({ key: "twitch_channels", event_type: request.event_type, delivery: "EVENT_SOURCE", channel_id: broadcaster_id, discord_server: request.discord_server }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-    }
-})
+type SubscriptionResponse = { data: Array<{ id: string, status: string, type: string, version: string, cost: number, condition: { broadcaster_user_id: string }, transport: { method: string, callback: string }, created_at: string }>, total: number, total_cost: number, max_total_cost: number }
 
+type SubscriptionDoc = { subscriptionId: string, servers: { [key: string]: { subscribed: boolean } } }
 
-app.use(bodyParser({ enableTypes: ["json"], encoding: "utf-8" }))
-    .use(async (ctx, next) => {
+router.post("/events",
+    async (ctx, next) => {
         const secret = getSecret()
         const message = getHmacMessage(ctx)
         const hmac = HMAC_PREFIX + getHmac(secret, message)
@@ -204,8 +97,8 @@ app.use(bodyParser({ enableTypes: ["json"], encoding: "utf-8" }))
             console.log("signatures dont match")
             ctx.status = 403
         }
-    })
-    .use(async (ctx, next) => {
+    },
+    async (ctx, next) => {
         if (ctx.request.get(MESSAGE_TYPE) === MESSAGE_TYPE_VERIFICATION) {
             ctx.set({ "Content-Type": "text/plain" })
             ctx.status = 200
@@ -213,8 +106,8 @@ app.use(bodyParser({ enableTypes: ["json"], encoding: "utf-8" }))
         } else {
             await next()
         }
-    })
-    .use(async (ctx, next) => {
+    },
+    async (ctx, next) => {
         if (MESSAGE_TYPE_REVOCATION === ctx.request.get(MESSAGE_TYPE)) {
             ctx.status = 204
             console.log(ctx.request.rawBody)
@@ -230,7 +123,104 @@ app.use(bodyParser({ enableTypes: ["json"], encoding: "utf-8" }))
             }
 
         }
+    },
+    async (ctx, next) => {
+        const twitchEvent = ctx.request.body as StreamUpEvent
+        ctx.status = 200
+        await next()
+        const twitchUser = twitchEvent.event.broadcaster_user_id
+        const channelInformation = await twitchClient.retrieveChannelInformation(twitchUser)
+        const broadcaster = channelInformation.data[0]
+        const broadcasterName = broadcaster.broadcaster_name
+        const broadcastTitle = broadcaster.title
+        const subscriptionDoc = await db.collection("twitch_notifiers").doc(twitchUser).get()
+        if (!subscriptionDoc.exists) {
+            throw new Error(`Subscription for ${twitchUser} does not exist!`)
+        }
+        const subscription = subscriptionDoc.data() as SubscriptionDoc
+        const subscribedServers = Object.entries(subscription.servers).filter(entry => entry[1].subscribed).map(entry => entry[0])
+        await Promise.all(subscribedServers.map(async (server) => {
+            const res = await fetch("https://snallabot-event-sender-b869b2ccfed0.herokuapp.com/query", {
+                method: "POST",
+                body: JSON.stringify({ event_types: ["BROADCAST_CONFIGURATION"], key: server, after: 0, limit: 1 }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            const broadcastResponse = await res.json() as BroadcastConfigurationResponse
+            const sortedEvents = broadcastResponse.BROADCAST_CONFIGURATION.sort((a: BroadcastConfigurationEvent, b: BroadcastConfigurationEvent) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime())
+            if (sortedEvents.length === 0) {
+                console.error(`${server} is not configured for Broadcasts`)
+            } else {
+                const configuration = sortedEvents[0]
+                const titleKeyword = configuration.title_keyword
+                if (broadcastTitle.toLowerCase().includes(titleKeyword.toLowerCase())) {
+                    await fetch("https://snallabot-event-sender-b869b2ccfed0.herokuapp.com/post", {
+                        method: "POST",
+                        body: JSON.stringify({
+                            key: server, event_type: "MADDEN_BROADCAST", delivery: "EVENT_SOURCE", title: broadcastTitle, video: `https://www.twitch.tv/${broadcasterName}`,
+                            headers: {
+                                "Content-Type": "application/json"
+                            }
+                        })
+                    })
+                }
+            }
+        }))
+
+
+    }).post("/addTwitchNotifier", async (ctx, next) => {
+        const request = ctx.request.body as AddTwitchChannelRequest
+        const broadcasterInformation = await twitchClient.retrieveBroadcasterInformation(request.twitch_url)
+        const broadcasterId = broadcasterInformation.data[0].id
+        const currentSubscriptionDoc = await db.collection("twitch_notifiers").doc(broadcasterId).get()
+        if (currentSubscriptionDoc.exists) {
+            const currentSubscription = currentSubscriptionDoc.data()
+            await db.collection("twitch_notifiers").doc(broadcasterId).set(
+                {
+                    servers: {
+                        [request.discord_server]: { subscribed: true }
+                    }
+                }, { merge: true }
+            )
+        } else {
+            const subscription = await twitchClient.subscribeBroadcasterStreamOnline(broadcasterId)
+            const subscriptionId = subscription.data?.[0]?.id
+            if (!subscriptionId) {
+                throw new Error(`Subscription response not formed correctly: ${subscription}`)
+            }
+            await db.collection("twitch_notifiers").doc(broadcasterId).set({
+                subscriptionId: subscriptionId,
+                servers: {
+                    [request.discord_server]: { subscribed: true }
+                }
+            })
+        }
+        ctx.status = 200
+    }).post("/removeTwitchNotifier", async (ctx, next) => {
+        const request = ctx.request.body as RemoveTwitchChannelRequest
+        const broadcasterInformation = await twitchClient.retrieveBroadcasterInformation(request.twitch_url)
+        const broadcasterId = broadcasterInformation.data[0].id
+        const currentSubscriptionDoc = await db.collection("twitch_notifiers").doc(broadcasterId).get()
+        if (currentSubscriptionDoc.exists) {
+            const currentSubscription = currentSubscriptionDoc.data() as SubscriptionDoc
+            const numSubscribed = Object.entries(currentSubscription.servers).filter((entry) => entry[0] != request.discord_server && entry[1].subscribed).length
+            if (numSubscribed === 0) {
+                await twitchClient.deleteSubscription(currentSubscription.subscriptionId)
+                await db.collection("twitch_notifiers").doc(broadcasterId).delete()
+            } else {
+                await db.collection("twitch_notifiers").doc(broadcasterId).update({
+                    [`servers.${request.discord_server}`]: FieldValue.delete()
+                })
+            }
+        } else {
+            throw new Error(`Twitch notifier does not exist for ${request.twitch_url}. It may never have been added`)
+        }
+        ctx.status = 200
     })
+
+
+app.use(bodyParser({ enableTypes: ["json"], encoding: "utf-8" }))
     .use(router.routes())
     .use(router.allowedMethods())
 
